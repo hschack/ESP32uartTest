40135dc1633f9f74dd009799d12fa85ce26370b2
Henrik Schack <hschack@gmail.com>
2024-03-12 11:38:34 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define RxPin 16
+#define TxPin 17
+#define BAUDRATE 115200
+HardwareSerial SerialAT(1);
+
+
+void setup()
+{
+  Serial.begin(115200);
+  SerialAT.begin(BAUDRATE, SERIAL_8N1, RxPin, TxPin);
+  Serial2.begin(115200);
+
+}
+
+void loop()
+{
+  Serial.println("test port");
+  Serial1.println("test port 1");
+  Serial2.println("test port 2");
+  delay(1000); 
+}
