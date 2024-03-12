0fe25b883cd3a8e70c617bcad4af2c99d666ce05
Henrik Schack <hschack@gmail.com>
2024-03-12 11:57:40 +0100

    uart2Virker

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,21 @@
 #define RxPin 16
 #define TxPin 17
 #define BAUDRATE 115200
-HardwareSerial SerialAT(1);
+HardwareSerial SerialAT(2);
 
 
 void setup()
 {
   Serial.begin(115200);
   SerialAT.begin(BAUDRATE, SERIAL_8N1, RxPin, TxPin);
-  Serial2.begin(115200);
+  //Serial2.begin(115200);
 
 }
 
 void loop()
 {
   Serial.println("test port");
-  Serial1.println("test port 1");
-  Serial2.println("test port 2");
-  delay(1000); 
+  //Serial1.println("test port 1");
+  SerialAT.println("test port 2");
+  delay(500); 
 }
