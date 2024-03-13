16ddde8bcfa5d90d9380ff26c690f59c7a9472bf
Henrik Schack <hschack@gmail.com>
2024-03-13 13:08:06 +0100

    test_apn_v2

--- src/main.cpp
+++ src/main.cpp
@@ -31,35 +31,61 @@ void setup()
   
   SerialAT.println("AT+COPS?"); //(tdc= AT+COPS=1,2,"28301",9)
   delay(1000);
+  // +COPS: 0,2,"23801",9
   SerialAT.println("AT+CGPADDR"); //Request the assigned IP address
   delay(5000);
+  // +CGPADDR: 0,"10.9.18.110"
+  // +CGPADDR: 1
   SerialAT.println("AT+VIOSET=2"); //Set Module to 2.8V
   delay(1000);
   // OK
   SerialAT.println("AT+CFUN=0"); //Switch off the radio module
   delay(1000); 
+  // OK
   // +CEREG: 0
   SerialAT.println("AT+QCBAND=0,20"); //Set the frequency band to 20 for TDC (think can read)
   delay(1000);
   // OK
   SerialAT.println("AT+CFUN=1,1"); //Switch on the radio again
-  delay(5000);  
+  delay(5000); 
+  // SIM Ready
+
+  // +CEREG: 2
+
+  // Network Available
+
+  // +CEREG: 1 
+  
+  // OK
   SerialAT.println("AT+CGDCONT=0,\"IP\",\"internet\""); //Set APN AT+CGDCONT= 0 -> 10 også prævet med "IP"
   //SerialAT.println("AT+CGDCONT=1," + SettdcAPN); //Set APN AT+CGDCONT= 0 -> 10
   delay(5000);
   SerialAT.println("AT+CGCONTRDP"); //Auto connect !! vent på IP adr
-  delay(10000);  
+  delay(10000); 
+  // +CGCONTRDP: 0,5,"internet","10.136.240.222.255.255.255.0","10.136.240.222","194.239.134.83","193.162.153.164",,,,,1500
+  // OK   
   SerialAT.println("AT+CGPADDR"); //Request the assigned IP address
+  // +CGPADDR: 0,"10.136.240.222"
+  // +CGPADDR: 1
+
+  // OK
   delay(1000);
   SerialAT.println("AT+COPS?"); //(tdc= AT+COPS=1,2,"28301",9)
   delay(5000);   
+  // +COPS: 0,2,"23801",9
 
+  // OK
   SerialAT.println("AT+CGCONTRDP=?"); //check IP adr
   delay(5000);
+  // +CGCONTRDP: (0)
 
+  // OK
   SerialAT.println("AT+CGDCONT?"); //check IP adr
   delay(5000);
+  // +CGDCONT: 0,"IP","internet","10.136.240.222"
+  // +CGDCONT: 1,"IPV6","nbiot.tdc.dk"
 
+  // OK
 }
 
 void loop()
